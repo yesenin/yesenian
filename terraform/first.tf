282b2e0c5b463ea7e0a846c27e0de7c1cf3255df
Anton Yesenin <anton.yesenin@gmail.com>
2026-05-03 16:24:51 +0400

    fixes lambda_assume_role

--- terraform/first.tf
+++ terraform/first.tf
@@ -19,20 +19,16 @@ data "aws_iam_policy_document" "lambda_assume_role" {
         identifiers = ["lambda.amazonaws.com"]
     }
   }
-  statement {
-    effect = "Allow"
-    actions = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-    ]
-    resources = ["arn:aws:logs:*:*:*"]
-  }
 }
 
 resource "aws_iam_role" "commonRole" {
   name               = "lambda_execution_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.commonRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "getAllWords" {
@@ -42,4 +38,8 @@ resource "aws_lambda_function" "getAllWords" {
   runtime          = "nodejs22.x"
   filename         = "../src/dist/lambda.zip"
   source_code_hash = filebase64sha256("../src/dist/lambda.zip")
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic
+  ]
 }
