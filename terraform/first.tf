1dd8f8f75fe170a1586582bbbf6cf7199953271f
Anton Yesenin <anton.yesenin@gmail.com>
2026-05-03 14:49:29 +0400

    No depends_on

--- terraform/first.tf
+++ terraform/first.tf
@@ -35,11 +35,6 @@ resource "aws_iam_role" "commonRole" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.commonRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 resource "aws_lambda_function" "getAllWords" {
   function_name    = "getAllWords"
   role             = aws_iam_role.commonRole.arn
@@ -47,7 +42,4 @@ resource "aws_lambda_function" "getAllWords" {
   runtime          = "nodejs22.x"
   filename         = "../src/dist/lambda.zip"
   source_code_hash = filebase64sha256("../src/dist/lambda.zip")
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_basic
-  ]
 }
